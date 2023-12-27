578b997cbbf59666f00bf634815c1afd69698135
TMats <t.matsu.changmin@gmail.com>
2023-12-27 20:58:21 +0900

    Fix dependencies

--- docker/initialize-docker-container.sh
+++ docker/initialize-docker-container.sh
@@ -7,12 +7,12 @@ cd ~/workspace/third_party/RLBench && pip install -r requirements.txt && python 
 # install YARR
 cd ~/workspace/third_party/YARR && pip install -r requirements.txt && python setup.py develop
 # install GNFactor
-cd ~/workspace && pip install -r requirements.txt && python setup.py develop
+cd ~/workspace/GNFactor && pip install -r requirements.txt && python setup.py develop
 # install ODISE
 cd ~/workspace/third_party/ODISE && pip install -e .
 # error catching 
 # pip uninstall -y opencv-python
-pip install hydra-core==1.1 opencv-python-headless numpy==1.23.5
+pip install hydra-core==1.1 opencv-python-headless numpy==1.23.5 packaging==21.3
 cd ~/
 
 # https://stackoverflow.com/questions/30209776/docker-container-will-automatically-stop-after-docker-run-d
